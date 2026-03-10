fd071c4b73c195ad3e6177d2aa8da27e8f4e0a67
s35214-hue <s35214@pjwstk.edu.pl>
2026-03-10 18:01:24 +0100

    Displays the fibonacci value at a point

---
+++ src/EX2.java
@@ -0,0 +1,21 @@
+public class EX2 {
+
+    public static void main(String[] args) {
+
+        int n = 7;
+
+        System.out.println(fibonacci(n));
+
+    } public static int fibonacci(int n) {
+
+       if (n == 0) {
+           return 0;
+       } else if (n == 1) {
+           return 1;
+       } else {
+           return fibonacci(n - 1) + fibonacci(n - 2);
+       }
+
+    }
+
+}
